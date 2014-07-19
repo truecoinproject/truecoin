76876d354f4259f54ad5c59666999f3f7a19b5ea
dotcoin <dotcoin@live.com>
2014-07-19 18:36:07 +0800

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Truecoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "TruecoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
